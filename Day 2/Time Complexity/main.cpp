34d048207b52f7301a351a23fa4a80c879ea731d
Suryansh Jaiswal <82227776+LateDeveloperr@users.noreply.github.com>
2022-07-10 15:14:29 +0530

    Add files via upload

---
+++ Day 2/Time Complexity/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int count = 0 ;
+    for(int i=0;i>0;i/=2){
+        for(int j=0;j<i;j++){
+            count++;
+        }
+    }
+
+    return 0;
+}
