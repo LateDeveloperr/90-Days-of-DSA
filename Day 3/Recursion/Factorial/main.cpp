6d0b75099166ed2a9c4b2e85b04b40d9ac6cb391
Suryansh Jaiswal <82227776+LateDeveloperr@users.noreply.github.com>
2022-07-11 13:41:18 +0530

    Update main.cpp

--- Day 3/Recursion/Factorial/main.cpp
+++ Day 3/Recursion/Factorial/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int factorial(int n){
-    if(n==0 || n==1){
-        return n;
+    if(n==0 ){
+        return 1;
     }
     int prevFactorial = factorial(n-1);
     return n*prevFactorial;
